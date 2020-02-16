7e12347a54bf19180a4cbb86e72c172eaa05ed2e
Khaled Mohamed <khaledmzakria1@gmail.com>
2020-02-16 10:44:25 +0200

    Sign Up page added

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('register.html', title='Register', form= form)
     
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template('login.html', title= 'Login', form= form)
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
